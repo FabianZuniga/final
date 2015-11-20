1fd8b51b6b834878de117a13566e1fb636dd486c
Fabian <Fabian@FamiliaZuiga>
2015-11-20 10:38:04 -0500

    nuevo git

---
+++ proyectoFinalZRF/src/java/ImagesView1.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Fabian
+ */
+
+ 
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+ 
+@ManagedBean
+public class ImagesView1 {
+     
+    private List<String> images;
+     
+    @PostConstruct
+    public void init() {
+        images = new ArrayList<String>();
+        for (int i = 1; i <= 3; i++) {
+            images.add("eqp" + i + ".png");
+        }
+    }
+ 
+    public List<String> getImages() {
+        return images;
+    }
+}
